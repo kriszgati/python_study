fef220890f3970196fe92e896a48fd204191c060
Krisztian Gati <kgati@redhat.com>
2024-04-16 14:51:18 +0200

    First working flask application

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # python_study
+
+To create python virtual environment:
+python -m venv venv
+
+Activate the environment:
+venv\Scripts\activate       # in Win
+source venv/bin/activate    # in Linux
+
+Deactivate the environment:
+deactivate
+
+
+python -m pip install -r requirements.txt
+# -r means recursive, install also dependencies
+
+
--- my_webserver.py
+++ my_webserver.py
@@ -1,7 +1,20 @@
 from flask import Flask
 import requests
+from bs4 import BeautifulSoup
+import logging
 
 app = Flask(__name__)
+
+# Set the log level
+app.logger.setLevel(logging.INFO)
+
+# Define a file handler and set its format
+file_handler = logging.FileHandler('flask.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+# Add the file handler to the app's logger
+app.logger.addHandler(file_handler)
 
 @app.route("/")
 def hello_world():
@@ -11,7 +24,16 @@ def hello_world():
 def hello_world2():
     return "<p>Hello, World-2!</p>"
 
-@app.route("/scrapper")
+@app.route("/scrap")
 def my_scrapper():
-    web_page = requests.get("https://access.redhat.com/security/cve/CVE-2023-45803")
-    return web_page.content
+    response = requests.get("https://access.redhat.com/security/cve/CVE-2023-45803")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    app.logger.error(soup)
+    #with open('request_info.md', 'w') as f:
+    #    f.write(str(response.text))
+    return response.text
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
