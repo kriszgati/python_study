adc4e5206f365e64569af47dc53959437143bf83
Krisztian Gati <kgati@redhat.com>
2024-06-08 22:12:49 +0200

    Search for the input file, entry point log and many other logs, timestamped output filename features added

--- my_webserver.py
+++ my_webserver.py
@@ -4,6 +4,8 @@ import requests
 import json
 import csv
 import logging
+import datetime
+import os
 
 
 app = Flask(__name__)
@@ -31,30 +33,41 @@ def hello_world2():
 
 @app.route("/scrap")
 def my_scrapper():
-    # TODO 2. read the cve number from xlsx file
+    # TODO Replace all constants with the config variable
+    # TODO Search for the input file
+    # TODO Add an entry point log
+    # TODO Add the timestamp to the output file name
+    # TODO Use xlsx file instead of csv file
+
+    app.logger.info(f'*******************************')
+    app.logger.info(f'**  The scrapper is running  **')
+    app.logger.info(f'*******************************')
 
     config = load_config()
-    csv_file_path = config.get('CVE', 'CVE_FILE_NAME')
+    input_file_name_pattern = config.get('CVE_INPUT', 'FILE_PREFIX')
+    directory = config.get('CVE_INPUT', 'SEARCH_PATH')
     cve_list = []
+
+    matches = find_files(directory, input_file_name_pattern)
+    app.logger.info(f'The following files are found: {matches}')
+    last_file = matches[-1]
+    app.logger.info(f'The last file in the list is: {last_file}')
+    first_file = matches[0]
+    app.logger.info(f'The first file in the list is: {first_file}')
     
-    with open(csv_file_path, newline='') as csvfile:
+    with open(first_file, newline='') as csvfile:
         csv_content = csv.reader(csvfile)
-        next(csv_content)   # Skip the first line in the following 'for' loop
+        next(csv_content)   # Skip the first line (the header line) in the following 'for' loop
         for row in csv_content:
-            app.logger.info('Row: ' + str(row))
+            app.logger.info(f'Row: {row}')
             cve_list.append(row[0])
-            app.logger.info('CVE list: ' + str(cve_list))
+            app.logger.info(f'CVE list: {cve_list}')
 
-    # TODO Replace all constants with the config variable
-    # TODO search for the xlsx file
+    # Create the output filename with a timestamp prefix
+    output_filename = create_output_filename(first_file)
 
-#    cve_list = config.get('CVE', 'CVE_LIST')
-#    app.logger.info('CVE list to be parsed: ' + cve_list)
-#    cve_list = cve_list.split(',')
-#    app.logger.info('CVE list represented as list of strings: ' + str(cve_list))
-    csv_file_path = 'Output_'+csv_file_path
-    with open(csv_file_path, "a", newline="") as csvfile:
-        fieldnames = config.get('CVE', 'HEADER_FIELD_NAMES')
+    with open(output_filename, "a", newline="") as csvfile:
+        fieldnames = config.get('CVE_INPUT', 'HEADER_FIELD_NAMES')
         fieldnames = fieldnames.split(',')
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -82,8 +95,30 @@ def load_config():
     return config
 
 
+# Function to search for files that start with a given pattern in a directory
+def find_files(directory, pattern):
+    app.logger.info(f'Searching for files in the {directory} directory, starting with the following pattern: {pattern}')
+    matches = []
+    app.logger.info(f'The following files are found:')
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if filename.startswith(pattern):
+                app.logger.info(filename)
+                matches.append(os.path.join(root, filename))
+    return matches
 
 
+# Function to create an output filename with a timestamp prefix
+def create_output_filename(input_filename):
+    app.logger.info(f'Creating file name from the current time and the input file name')
+    current_time = datetime.datetime.now()
+    app.logger.info(f'The current time is: {current_time}')
+    timestamp = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    app.logger.info(f'The timestamp is: {timestamp}')
+    directory, filename = os.path.split(input_filename)
+    output_filename = f"{timestamp}_{filename}"
+    app.logger.info(f'The output file name is: {output_filename}')
+    return os.path.join(directory, output_filename)
 
 
 
